ded2a3c4cae6b542f2e0216f1886d5bf61f682bd
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-10-15 11:56:02 +0500

    new mini project

---
+++ rendomColor/index.js
@@ -0,0 +1,29 @@
+// genrate rendom color
+
+const randomColor  = function () {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)]
+    }    
+    return color;
+};
+let intervel
+// console.log(randomColor())
+const startChangingColor = function(){
+   
+    intervel = setInterval(changingColor, 1000)
+   function changingColor(){
+    document.body.style.background = randomColor()
+ }
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervel)
+}
+
+document.querySelector("#start").
+addEventListener("click",startChangingColor)
+
+document.querySelector("#stop").
+addEventListener("click",stopChangingColor)
